613307dc155aece1c9ed18273b504f210e1d3aa3
ThikerXCX <Rifkimuhammad967@gmail.com>
2022-01-18 14:40:32 +0700

    entahlah

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { DivisionComponent } from './division/division.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    DivisionComponent
   ],
   imports: [
     BrowserModule,
